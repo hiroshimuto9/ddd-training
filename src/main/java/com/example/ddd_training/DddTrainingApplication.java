fc5f0e5648ec6aa9ee5d56256a8aecc8a38b8628
mutohiroshi <mutohiroshi@team-lab.com>
2021-01-02 17:00:23 +0900

    first commit

---
+++ src/main/java/com/example/ddd_training/DddTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddd_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddTrainingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DddTrainingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/ddd_training/DddTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddd_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
