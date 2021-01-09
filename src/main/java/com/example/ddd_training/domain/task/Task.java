02cde2ef7dad0e842e21f63fa336b7debc009b09
mutohiroshi <mutohiroshi@team-lab.com>
2021-01-09 16:40:23 +0900

    add: 期限延期処理の作成

--- src/main/java/com/example/ddd_training/domain/task/Task.java
+++ src/main/java/com/example/ddd_training/domain/task/Task.java
@@ -22,4 +22,15 @@ public class Task {
     // タスク作成時は、ステータスが未完了で作成される。
     this.taskStatus = TaskStatus.UNDONE;
   }
+
+  private static final int MAX_POSTPONE_COUNT = 3;
+  // 期限の延期処理
+  // Setterを使っていないため、コンストラクタの状態変更はこの処理を通さないと行えないと判断しやすい。
+  public void postpone() {
+    if (postponeCount >= MAX_POSTPONE_COUNT) {
+      throw new IllegalArgumentException("最大延期回数を超えています");
+    }
+    dueDate.plusDays(1L);
+    postponeCount++;
+  }
 }
